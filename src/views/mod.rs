7de286fa676f4a701c640442e51a8733dede862a
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2022-10-11 19:13:59 +0200

    Revert "Refactoring"
    This reverts commit b8f95c91be44454b310c1047caa5b87813b1363a.

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -18,7 +18,6 @@ mod session;
 mod setting;
 pub mod transaction;
 mod user;
-mod utils;
 mod work;
 
 use crate::views::path::Path;
--- src/views/work.rs
+++ src/views/work.rs
@@ -1,5 +1,6 @@
 use crate::error::*;
 use crate::item::atoi;
+use crate::item::RentalSetting;
 use crate::item::{search_item, search_items, update_item};
 use crate::item::{Book, BorrowedBook, User};
 use crate::views::cache::*;
@@ -7,7 +8,6 @@ use crate::views::db_helper::get_db;
 use crate::views::reply::Reply;
 use crate::views::session::*;
 use crate::views::transaction::*;
-use crate::views::utils::get_rental_setting;
 use actix_session::Session;
 use actix_web::{web, HttpResponse, Result};
 use log::{debug, error};
@@ -70,12 +70,20 @@ pub async fn process(
         }
     };
 
-    let setting = get_rental_setting(&db).await;
-    if setting.is_err() {
-        database::disconnect(&data);
-        return Err(BibErrorResponse::DataNotFound("RentalSetting".to_string()));
+    let mut setting = RentalSetting::default();
+    setting.id = 1;
+    let mut setting = match search_items(&db, &setting).await {
+        Ok(setting) => setting,
+        Err(e) => {
+            database::disconnect(&data);
+            return Err(BibErrorResponse::DataNotFound(e.to_string()));
+        }
+    };
+
+    if setting.len() != 1 {
+        return Err(BibErrorResponse::DataDuplicated);
     }
-    let setting = setting.unwrap();
+    let setting = setting.pop().unwrap();
 
     if form.borrowed_book_id != "" {
         borrow_book(
