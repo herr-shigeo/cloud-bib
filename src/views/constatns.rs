a128b3bd289633050be378e06b62c92a8bea6647
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2023-02-08 21:39:53 +0100

    Switch the billing to the production

--- src/views/constatns.rs
+++ src/views/constatns.rs
@@ -7,10 +7,10 @@ pub const NUM_TRANSACTIONS_FOR_FREE: u32 = 5000;
 pub const NUM_USERS_FOR_LIGHT: u32 = 100;
 pub const NUM_BOOKS_FOR_LIGHT: u32 = 1000;
 pub const NUM_TRANSACTIONS_FOR_LIGHT: u32 = 10000;
-pub const PRICE_ID_FOR_LIGHT: &str = "price_1MYz6nIxjInwoAs1NQ5STp78";
+pub const PRICE_ID_FOR_LIGHT: &str = "price_1MZKKoIxjInwoAs1GONVtTiq";
 
 // 10,000 yen
 pub const NUM_USERS_FOR_STANDARD: u32 = 1000;
 pub const NUM_BOOKS_FOR_STANDARD: u32 = 10000;
 pub const NUM_TRANSACTIONS_FOR_STANDARD: u32 = 50000;
-pub const PRICE_ID_FOR_STANDARD: &str = "price_1MYz7qIxjInwoAs1H5AT4E2S";
+pub const PRICE_ID_FOR_STANDARD: &str = "price_1MZKKgIxjInwoAs1eRWdKigE";
